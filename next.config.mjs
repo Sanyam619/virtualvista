9051053c1bb5ab04d3a4505ddb773a69f6daa70c
Sanyam Gupta <sanyamgupta@Sanyams-MacBook-Air.local>
2024-07-02 23:16:50 +0530

    fifth commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  },
 };
 
 export default nextConfig;
